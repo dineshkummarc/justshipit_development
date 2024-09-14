220083aca3343d2c50bb2332fa822f7ce160e883
Dinesh Kumar <mapmydestination@gmail.com>
2024-09-14 23:23:01 +0530

    update

---
+++ qbdesktop/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'consolibyte/quickbooks',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'b574a03c5a4e9abd15d818e9d7cbfd47509f328d',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'consolibyte/quickbooks' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'b574a03c5a4e9abd15d818e9d7cbfd47509f328d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
